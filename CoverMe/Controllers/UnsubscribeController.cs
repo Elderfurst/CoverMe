d4622a3c3131e6231e91fe9b94952c53ca9c3463
Nicholas Anderson <njcanderson@gmail.com>
2019-12-29 15:20:53 -0800

    Move unsubscribe page to separate controller/folder

---
+++ CoverMe/Controllers/UnsubscribeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CoverMe.Controllers
+{
+    public class UnsubscribeController : Controller
+    {
+        public UnsubscribeController() { }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
